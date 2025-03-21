62dc3c4b0a9b738f5dc5c6665fbf0b1314099562
vinay011201 <rvinayak313@gmail.com>
2025-03-21 17:49:47 +0530

    Commited java file

---
+++ Fjavaprogram.java
@@ -0,0 +1,22 @@
+public class demo
+{
+public static void main(String args[])
+{
+    String movie="Gadar 2";
+
+		if(movie.equals("pushpa 2"))
+		{
+			System.out.println("Not interested");
+		}
+		else if(movie.equals("Marco"))
+		{
+			System.out.println("Already Watched");
+		}
+		else if (movie.equals("Gadar 2"))
+		{
+			System.out.println("Trending movie");
+		}
+		else {
+			System.out.println("My Favorite Movie");
+		}
+	}
